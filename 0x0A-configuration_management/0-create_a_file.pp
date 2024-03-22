f45b7387c163b28e1566423b369d69f28074c758
Lina Abdelkhalig <lelsammani0@gmail.com>
2024-03-23 01:55:27 +0200

    task 0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# puppet manifest to create a file
+
+file { '/tmp/school':
+  ensure  => file,
+  path    => '/tmp/school1',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet\n',
+}
