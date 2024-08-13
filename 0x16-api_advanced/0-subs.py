96d1bf977a454f91fea26a5ab5811d99c7aadac7
Lina Abdelkhalig <lelsammani0@gmail.com>
2024-08-13 21:42:05 +0300

    task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+the module contains a function that
+queries the reddit api and returns the number of subscribers for ta given sub
+"""
+
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """this function uses the reddit api
+        to return the number of subsscribers
+        subscribed to a particular "subreddit"
+
+    Args:
+        subreddit (str): name of the subreddit
+
+    Returns:
+        int: the number of subscribers if successful or 0
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'my-reddit-subscriber-counter'}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json().get("data")
+        return data.get("subscribers")
+    return 0
